54a564471785cd9432082a8cd2e6cb3b29cba6ac
GreciaJhoanaMartinez <125307084+GreciaJhoanaMartinez@users.noreply.github.com>
2024-10-22 14:34:07 -0600

    Update connection.php

--- CRUD_Testing_DPS/connection.php
+++ CRUD_Testing_DPS/connection.php
@@ -1,20 +1,24 @@
 <?php
 
 function connection(){
-    $host = "USUARIO FINAL";
+    $host = "Localhost";
     $user = "root";
     $pass = "";
-    $bd = "users";
 
     $bd = "usuarios";
 
     $connect=mysqli_connect($host, $user, $pass);
+    if (!$connect) {
+        die("Error de conexión: " . mysqli_connect_error());
+    }
 
     mysqli_select_db($connect, $bd);
-
+if (!mysqli_select_db($connect, $bd)) {
+        die("Error al seleccionar la base de datos: " . mysqli_error($connect));
+    }
     return $connect;
 
 }
 
-
 ?>
+
