8bfb32f2f0561f7418eabcd6a20c39f386f49c8d
FannyVA <125307287+FannyVA@users.noreply.github.com>
2024-10-22 09:41:52 -0600

    Update connection.php

--- CRUD_Testing_DPS/connection.php
+++ CRUD_Testing_DPS/connection.php
@@ -4,8 +4,9 @@ function connection(){
     $host = "USUARIO FINAL";
     $user = "root";
     $pass = "";
+    $bd = "users";
 
-    $bd = "usuaries";
+    $bd = "usuarios";
 
     $connect=mysqli_connect($host, $user, $pass);
 
