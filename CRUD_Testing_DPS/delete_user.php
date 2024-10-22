6c0d668b4635df1b051966c5ba54a4f1bcbf38f9
ISC Emmanuel T.C. <true.xiyo@gmail.com>
2024-10-22 09:27:12 -0600

    Update delete_user.php

--- CRUD_Testing_DPS/delete_user.php
+++ CRUD_Testing_DPS/delete_user.php
@@ -5,7 +5,7 @@ $con = connection();
 
 $id=$_GET["id"];
 
-$sql="DELETE FROM usuarios WHERE id_usuario='$id'";
+$sql="CREATE FROM usuarios WHERE id_usuario='$id'";
 $query = mysqli_query($con, $sql);
 
 if($query){
