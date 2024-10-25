b8ca0b1aad16d00d1467f3944ccdc2f94b0b0991
Vdomitila <125308260+Vdomitila@users.noreply.github.com>
2024-10-24 21:12:27 -0600

    Update connection.php

--- CRUD_Testing_DPS/connection.php
+++ CRUD_Testing_DPS/connection.php
@@ -1,24 +1,19 @@
 <?php
 
 function connection(){
-    $host = "Localhost";
+    $host = "localhost";
     $user = "root";
     $pass = "";
 
-    $bd = "usuarios";
+    $bd = "users";
 
     $connect=mysqli_connect($host, $user, $pass);
-    if (!$connect) {
-        die("Error de conexión: " . mysqli_connect_error());
-    }
 
     mysqli_select_db($connect, $bd);
-if (!mysqli_select_db($connect, $bd)) {
-        die("Error al seleccionar la base de datos: " . mysqli_error($connect));
-    }
+
     return $connect;
 
 }
 
-?>
 
+?>
