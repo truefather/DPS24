06e567420a18f878c77578c0fe889f6538593791
GreciaJhoanaMartinez <125307084+GreciaJhoanaMartinez@users.noreply.github.com>
2024-10-24 22:04:12 -0600

    Update insert_user.php

--- CRUD_Testing_DPS/insert_user.php
+++ CRUD_Testing_DPS/insert_user.php
@@ -1,20 +1,26 @@
 <?php
+<<?php
 include("connection.php");
+nclude("connection.php");
 $con = connection();
 
 $id_producto = $_POST['id_producto'];
+$id_producto = $_POST['id_tester'];
+$id= $_POST['id_tester'];
 $nombre = $_POST['nombre'];
 $marca = $_POST['marca'];
 $precio = $_POST['precio'];
 
 
-$sql = "INSERT INTO productos VALUES('$id_producto','$nombre','$marca','$precio')";
+$sql = "INSERT INTO tester VALUES('$id_tester','$nombre','$apellido','$edad')";
+$sql = "INSERT INTO tester VALUES('$id_tester','$nombre','$apellido','$edad')";
+$sql = "INSERT INTO tester VALUES('$id_tester','$nombre','$apellido','$edad')";
+$sql = "INSERT INTO testers VALUES('$id_tester','$nombre','$apellido','$edad')";
 $query = mysqli_query($con, $sql);
 
 if($query){
     Header("Location: index.php");
 }else{
+@@ -17,4 +17,4 @@
 
 }
-
-?>
