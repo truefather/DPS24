65cb3768939b3c058046d690e3434026fbbfb184
FannyVA <125307287+FannyVA@users.noreply.github.com>
2024-10-25 15:25:02 -0600

    Update insert_user.php

--- CRUD_Testing_DPS/insert_user.php
+++ CRUD_Testing_DPS/insert_user.php
@@ -1,26 +1,21 @@
 <?php
-<<?php
 include("connection.php");
-nclude("connection.php");
 $con = connection();
 
-$id_producto = $_POST['id_producto'];
 $id_producto = $_POST['id_tester'];
 $id= $_POST['id_tester'];
 $nombre = $_POST['nombre'];
-$marca = $_POST['marca'];
-$precio = $_POST['precio'];
+$apellido = $_POST['apellido'];
+$edad = $_POST['edad'];
 
 
 $sql = "INSERT INTO tester VALUES('$id_tester','$nombre','$apellido','$edad')";
-$sql = "INSERT INTO tester VALUES('$id_tester','$nombre','$apellido','$edad')";
-$sql = "INSERT INTO tester VALUES('$id_tester','$nombre','$apellido','$edad')";
-$sql = "INSERT INTO testers VALUES('$id_tester','$nombre','$apellido','$edad')";
+
 $query = mysqli_query($con, $sql);
 
 if($query){
     Header("Location: index.php");
 }else{
-@@ -17,4 +17,4 @@
+@@ -17,4 +17,4 @
 
 }
